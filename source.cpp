e83dedbfd105675558b21b61326f6b83ca98cd3e
DarkVader135 <82936530+DarkVader135@users.noreply.github.com>
2021-04-21 09:38:06 +0100

    this is the source code

---
+++ source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<string>
+
+using namespace std; // spoiler tag programe
+string sentence;
+int i=3;
+int main () {
+    
+    getline(cin, sentence);
+    sentence.insert(0,"||");
+    int size = sentence.size();
+
+    for(;i<size;){
+        
+        sentence.insert(i, "||||");
+        size = sentence.size();
+        i += 5;
+    }
+    
+    sentence += "||" ;
+
+    cout << sentence << endl;
+    return 0;
+}
